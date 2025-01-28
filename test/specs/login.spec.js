395bc71fef57ce127000175933177ea5f60e838d
Anton Kolupaiev <antnikol@gmail.com>
2025-01-28 19:04:52 +0200

    add faker library + Test_case #3

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -2,7 +2,9 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import InvertoryPage from '../pageobjects/invertory.page.js'
 import data from '../fixtures/data.json'
+import { genTestData } from '../fixtures/genData.js'
 
+const genData = genTestData()
 
 
 describe('Login Page tests suite', () => {
@@ -19,8 +21,18 @@ describe('Login Page tests suite', () => {
     await expect(InvertoryPage.getCartIcon()).toBeDisplayed()
   })
 
+  it('Login with the user valid login and the invalid password', async () => {
+    await LoginPage.login(data.user.name, genData.userPassword)
+
+    await expect(await LoginPage.getLoginErrorIcon()).toBeDisplayed()
+    await expect(await LoginPage.getPasswordErrorIcon()).toBeDisplayed()
+    await expect(await LoginPage.getInputUsername()).toHaveClass('error')
+    await expect(await LoginPage.getInputPassword()).toHaveClass('error')
+    await expect(await LoginPage.getEpicErrorMessage()).toHaveText(data.message.loginError)
+  })
+
   it('Login with the user invalid login and the valid password', async () => {
-    await LoginPage.login(data.user.name, data.user.invalid_password)
+    await LoginPage.login(genData.userName, data.user.password)
 
     await expect(await LoginPage.getLoginErrorIcon()).toBeDisplayed()
     await expect(await LoginPage.getPasswordErrorIcon()).toBeDisplayed()
